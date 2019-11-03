a66d72f093a281c5eae784ffc7a1b6a7047d9901
froseror <froseror@unal.edu.co>
2019-11-03 02:10:23 -0500

    Get value from user/Print array/Print index

--- arrays2.cpp
+++ arrays2.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <iterator> 
 
 int main()
 {
+  //Get value from user:
+  
+  int number = 0;
+  do
+    {
+      std::cout << "Enter an integer number between 1 and 9: ";
+      std::cin >> number;
+
+      if (std::cin.fail())
+	std::cin.clear();
+
+      std::cin.ignore(32767, '\n');
+    }
+  while (number < 1 or number > 9);
+
+  //Print array:
+  
+    int array[] = {4, 6, 7, 3, 8, 2, 1, 9, 5};
+  const int arrayLength = std::size(array);
+  
+  for (int index = 0; index < arrayLength; ++index){
+    std::cout << array[index] << "\n";
+  }
+
+  //Print the index:
+
+    for (int INDEX = 0; INDEX < arrayLength; ++INDEX){
+      if (array[INDEX] == number){
+	std::cout << "The index is: " << INDEX << std::endl;
+	break;
+      }
+    }
+    
   return 0;
 }
